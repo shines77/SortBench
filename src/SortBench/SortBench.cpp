d6ce8e9acb91a5e62b905a87e662fa907731545a
GuoXH <gz_shines@msn.com>
2023-03-17 10:04:36 +0800

    调整宽域随机测试的顺序
    Signed-off-by: GuoXH <gz_shines@msn.com>

--- src/SortBench/SortBench.cpp
+++ src/SortBench/SortBench.cpp
@@ -434,7 +434,7 @@ void sort_benchmark_impl()
     sort_algo_bench<Algorithm::ska_sort_copy,  T>(TEST_PARAMS(test_array_list));
     sort_algo_bench<Algorithm::jstdBucketSort, T>(TEST_PARAMS(test_array_list));
 
-    // Forced test wide field array
+    // Test wide range random array
     for (size_t i = 0; i < array_count; i++) {
         std::vector<T> & test_array = test_array_list[i];
         size_t length = test_array.size();
@@ -445,9 +445,9 @@ void sort_benchmark_impl()
     }
     generate_standard_answers<T>(standard_answers, test_array_list, array_count);
 
-    sort_algo_bench<Algorithm::jstdBucketSortWide, T>(TEST_PARAMS(test_array_list));
     sort_algo_bench<Algorithm::ska_sort_wide,      T>(TEST_PARAMS(test_array_list));
     sort_algo_bench<Algorithm::ska_sort_copy_wide, T>(TEST_PARAMS(test_array_list));
+    sort_algo_bench<Algorithm::jstdBucketSortWide, T>(TEST_PARAMS(test_array_list));
 
     printf("\n");
 #undef TEST_PARAMS
